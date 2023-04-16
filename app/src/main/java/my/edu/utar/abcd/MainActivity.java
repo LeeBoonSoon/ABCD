5e9f1d3ea652701a94e669114851dc0ff57ca3ba
LeeBoonSoon <117886979+LeeBoonSoon@users.noreply.github.com>
2023-04-16 14:53:05 +0800

    Done bottom_nav_menu

---
+++ app/src/main/java/my/edu/utar/abcd/MainActivity.java
@@ -0,0 +1,58 @@
+package my.edu.utar.abcd;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import android.widget.Button;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    BottomNavigationView bottomNavigationView;
+
+    homeFragment homeF = new homeFragment();
+    profileFragment profileF = new profileFragment();
+    searchFragment searchF = new searchFragment();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setSelectedItemId(R.id.person);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.person:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.flFragment, homeF)
+                        .commit();
+                return true;
+
+            case R.id.home:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.flFragment, profileF)
+                        .commit();
+                return true;
+
+            case R.id.search:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.flFragment, searchF)
+                        .commit();
+                return true;
+        }
+        return false;
+    }
+}
